634fa2267614f71c683ef48fa2c88088ba0ee3b2
Nicolas Cabrera <nicocabrera.0212@gmail.com>
2021-10-16 20:42:27 -0300

    editamos el archivo app.js

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
+import RecoveryPassword from '../containers/RecoveryPassword';
+import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
+import '../styles/global.css';
 
-
-import '../style/global.scss'
-import Layaut from '../containers/Layout'
-import Login from '../containers/Login'
 const App = () => {
     return (
-        <Layaut>
-            <Login/>
-        </Layaut>
+        <BrowserRouter>
+            <Layout>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/Login" component={Login} />
+                    <Route exact path="/recovery-password" component={RecoveryPassword} />
+                    <Route component={NotFound} />
+
+                </Switch>
+            </Layout>
+        </BrowserRouter>
     );
 }
 
