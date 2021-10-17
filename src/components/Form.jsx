b87e98f953dd63ffee54dd43f896c4f2e64bbdd5
Nicolas Cabrera <nicocabrera.0212@gmail.com>
2021-10-16 23:46:51 -0300

    Modificamos el archivo de CreateAccount.jsx creando el archivo Form.jsx u sus estilos

---
+++ src/components/Form.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../styles/Form.scss';
+import Input from "../components/Input";
+import ButtomPrimary from "../components/ButtomPrimary";
+
+
+const Form = (values) => {
+    return (
+        <form action={values.actionForm} className={values.classnameForm}>
+          <div>
+            <Input
+              label="Name"
+              IdInput="Name"
+              TypeInput="text"
+              PlaceholderInput="Tu Nombre"
+              className="input input-text"
+            />
+            <Input
+              label="Email Addres"
+              IdInput="email"
+              TypeInput="email"
+              PlaceholderInput="example@hotmail.com"
+              className="input input-text"
+            />
+            <Input
+              label="Password"
+              IdInput="new-password"
+              TypeInput="password"
+              PlaceholderInput="********"
+              className="input input-password"
+            />
+          </div>
+          <ButtomPrimary valor="Create Account :)" />
+        </form>
+    )
+}
+
+export default Form
--- src/containers/CreateAccount.jsx
+++ src/containers/CreateAccount.jsx
@@ -1,23 +1,16 @@
 import React from "react";
 
-import ButtomPrimary from "../components/ButtomPrimary";
-import Input from "../components/Input";
+
+import Form from "../components/Form";
 import Title from "../components/Title";
-import '../styles/CreateAccount.scss';
+import "../styles/CreateAccount.scss";
 
 const CreateAccount = () => {
   return (
     <div className="login--container">
       <div className="form-container">
-        <Title classTitle="title" title="My new account"/>
-        <form action="/" className="form">
-          <div>
-            <Input label="Name" IdInput="Name" TypeInput="text" PlaceholderInput="Tu Nombre" className="input input-text"/>
-            <Input label="Email Addres" IdInput="email" TypeInput="email" PlaceholderInput="example@hotmail.com" className="input input-text"/>
-            <Input label="Password" IdInput="new-password" TypeInput="password" PlaceholderInput="********" className="input input-password"/>
-          </div>
-          <ButtomPrimary valor="Create Account :)"/>
-        </form>
+        <Title classTitle="title" title="My new account" />
+        <Form actionForm="/" classnameForm="form" />
       </div>
     </div>
   );
