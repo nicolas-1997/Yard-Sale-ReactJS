de67847faac6b472751a6a7f21ebdedca6d6c70e
Nicolas Cabrera <nicocabrera.0212@gmail.com>
2021-10-17 23:04:58 -0300

    Creamos el componente ShortDescription.jsx y sus estilos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Img from '../components/Img';
 import Price from '../components/Price';
-import EmailSent from '../containers/EmailSent';
+import ShortDesciption from '../components/ShortDesciption';
 
 const Home = () => {
 	return (
@@ -11,6 +11,8 @@ const Home = () => {
 			<Img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Bike" />
 			<Img src="https://www.hola.com/imagenes/estar-bien/20201104178485/consejos-gatos-para-principiantes/0-884-859/gatito-m.jpg" alt="Gato" />
 			<Price ClassName="price" price="$1500"/>
+			<ShortDesciption ClassName='ShortDescription' text="Bike"/>
+			<ShortDesciption ClassName='ShortDescription' text="Gatito"/>
 		</div>
 	);
 }
