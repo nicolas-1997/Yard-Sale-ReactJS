141dabf59901607606b3a7b046eda53507c295f5
Nicolas Cabrera <nicocabrera.0212@gmail.com>
2021-10-19 11:45:58 -0300

    dejamos una plantilla en blanco de useState para usarla mas tarde

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '@styles/ProductItem.scss';
 
 import btAddToCart from '@icons/bt_add_to_cart.svg'
+
+const ProductItem = () => {
+	const [cart, setCart] = useState([])
+}
+
+const handleClick = () =>{
+	setCart([])
+}
 
 const ProductItem = () => {
 	return (
@@ -12,7 +20,7 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
+				<figure onClick={handleClick} >
 					<img src={btAddToCart} alt="" />
 				</figure>
 			</div>
